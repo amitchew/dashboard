3bac5a388a408a599e00ba6073b58fff95178163
amitchew <49229715+amitchew@users.noreply.github.com>
2025-07-15 09:21:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <Users className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">333</div>
+            <div className="text-2xl font-bold">346</div>
             <p className="text-xs text-muted-foreground">+20% from last week</p>
           </CardContent>
         </Card>
